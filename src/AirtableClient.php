bab4b6201ef6c44d5e2f5dae3402527f247522df
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2021-05-24 16:13:42 +0200

    add findById

--- src/AirtableClient.php
+++ src/AirtableClient.php
@@ -13,7 +13,7 @@ class AirtableClient
         $this->id = $id;
     }
 
-    public function getData(string $table, ?string $view = null)
+    public function findAll(string $table, ?string $view = null)
     {
         $client = HttpClient::create();
         $convert = new JsonToArray();
@@ -27,5 +27,17 @@ class AirtableClient
         ]);
 
         return $convert->convert($response->getContent())['records'];
+    }
+
+    public function findById(string $table, string $id)
+    {
+        $client = HttpClient::create();
+        $convert = new JsonToArray();
+
+        $response = $client->request('GET', 'https://api.airtable.com/v0/'. $this->id .'/'. $table . '/' . $id, [
+            'auth_bearer' => $this->key,
+        ]);
+
+        return $convert->convert($response->getContent());
     }
 }
