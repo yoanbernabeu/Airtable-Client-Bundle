7a354e5ca2d04724c1efa4e58c56204925019516
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2021-05-25 15:14:04 +0200

    phpcs fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -3,6 +3,7 @@ namespace Yoanbernabeu\AirtableClientBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+
 class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
