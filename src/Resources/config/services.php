01ed7baa5e1643b54e4764709bd8be24f5656b2a
TBoileau <t-boileau@email.com>
2021-05-31 09:45:15 +0200

    feat: make services public in order to test it in functional tests

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -13,18 +13,21 @@ declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Yoanbernabeu\AirtableClientBundle\AirtableClient;
+use Yoanbernabeu\AirtableClientBundle\AirtableClientInterface;
 
 return static function (ContainerConfigurator $container): void {
     $container = $container->services()->defaults()
-        ->private()
+        ->public()
         ->autoconfigure()
         ->autowire()
     ;
 
-    $container->set(AirtableClient::class)
+    $container->set(AirtableClient::class, AirtableClient::class)
         ->args([
             '%yoanbernabeu_airtable_client.airtable_client.key%',
             '%yoanbernabeu_airtable_client.airtable_client.id%',
         ])
     ;
+
+    $container->alias(AirtableClientInterface::class, AirtableClient::class);
 };
