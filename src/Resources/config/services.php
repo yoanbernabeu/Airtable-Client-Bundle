dfb763a20194eb2b5673acf62f4d2d86566087b1
Florent Morselli <florent@morselli.fr>
2021-05-31 08:33:56 +0200

    Fix service definition

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -2,17 +2,11 @@
 
 declare(strict_types=1);
 
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2014-2020 Spomky-Labs
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
-
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Yoanbernabeu\AirtableClientBundle\AirtableClient;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container): void {
     $container = $container->services()->defaults()
@@ -25,6 +19,8 @@ return static function (ContainerConfigurator $container): void {
         ->args([
             '%yoanbernabeu_airtable_client.airtable_client.key%',
             '%yoanbernabeu_airtable_client.airtable_client.id%',
+            service(HttpClientInterface::class),
+            service(ObjectNormalizer::class),
         ])
     ;
 };
