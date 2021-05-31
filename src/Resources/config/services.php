98ea02423449265420b78e1be1433e615d4d3951
TBoileau <t-boileau@email.com>
2021-05-31 12:19:26 +0200

    refactor: delete unnecessary references in services.php for AirtableClient

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -3,12 +3,8 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Yoanbernabeu\AirtableClientBundle\AirtableClient;
 use Yoanbernabeu\AirtableClientBundle\AirtableClientInterface;
-
-use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container): void {
     $container = $container->services()->defaults()
@@ -20,9 +16,7 @@ return static function (ContainerConfigurator $container): void {
     $container->set('airtable_client', AirtableClient::class)
         ->args([
             '%yoanbernabeu_airtable_client.airtable_client.key%',
-            '%yoanbernabeu_airtable_client.airtable_client.id%',
-            service(HttpClientInterface::class),
-            service(ObjectNormalizer::class),
+            '%yoanbernabeu_airtable_client.airtable_client.id%'
         ])
     ;
 
