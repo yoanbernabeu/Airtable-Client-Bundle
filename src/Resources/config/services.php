dc08426b3ac594e5a67b634071db1eb0c4d7a37b
TBoileau <t-boileau@email.com>
2021-05-31 12:59:30 +0200

    refactor: use method chaining

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -7,18 +7,14 @@ use Yoanbernabeu\AirtableClientBundle\AirtableClient;
 use Yoanbernabeu\AirtableClientBundle\AirtableClientInterface;
 
 return static function (ContainerConfigurator $container): void {
-    $container = $container->services()->defaults()
+    $container->services()->defaults()
         ->public()
         ->autoconfigure()
         ->autowire()
-    ;
-
-    $container->set('airtable_client', AirtableClient::class)
+        ->set('airtable_client', AirtableClient::class)
         ->args([
             '%yoanbernabeu_airtable_client.airtable_client.key%',
             '%yoanbernabeu_airtable_client.airtable_client.id%'
         ])
-    ;
-
-    $container->alias(AirtableClientInterface::class, 'airtable_client');
+        ->alias(AirtableClientInterface::class, 'airtable_client');
 };
