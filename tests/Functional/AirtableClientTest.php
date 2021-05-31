8274f85022b37f4d163667828daf59cda6c6c2e6
TBoileau <t-boileau@email.com>
2021-05-31 11:26:26 +0200

    test: fix AirtableClientTest

--- tests/Functional/AirtableClientTest.php
+++ tests/Functional/AirtableClientTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Yoanbernabeu\AirtableClientBundle\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Yoanbernabeu\AirtableClientBundle\AirtableClient;
+use Yoanbernabeu\AirtableClientBundle\AirtableClientInterface;
 
 class AirtableClientTest extends KernelTestCase
 {
@@ -14,6 +14,7 @@ class AirtableClientTest extends KernelTestCase
         $kernel = self::bootKernel();
         $container = $kernel->getContainer();
 
-        $this->assertTrue($container->has(AirtableClient::class));
+        $this->assertTrue($container->has(AirtableClientInterface::class));
+        $this->assertTrue($container->has("airtable_client"));
     }
 }
